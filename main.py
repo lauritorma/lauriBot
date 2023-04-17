ff58f81407e3c0e5f3cf09f7d828576663fd39d7
lauritorma <bgu171@myy.haaga-helia.fi>
2023-04-17 12:45:18 +0300

    added greeting to new group members and some facts to facts.txt

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 
 import random
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import config
 
+#Random line from facts.txt
 
 def lauri_fakta(update, context):
     with open('facts.txt', 'r', encoding='utf-8') as f:
@@ -12,8 +13,26 @@ def lauri_fakta(update, context):
 
     context.bot.send_message(chat_id=update.message.chat_id, text=fact)
 
+# Greet new group member
+
+def greet_new_member(update, context):
+    for member in update.message.new_chat_members:
+        context.bot.send_message(chat_id=update.message.chat_id, text=f"Toivottakaa {member.first_name} tervetulleeksi LauriMafiaan! 🤝  \nJos haluat kuulla faktaa Laureista, kirjoita /laurifakta 👨‍🏫")
+
+
+# Updater for telegram bot 
+
 updater = Updater(token=config.API_KEY, use_context=True)
+
+# handler for random fact
 updater.dispatcher.add_handler(CommandHandler('laurifakta', lauri_fakta))
 
+# handler for new member greeting 
+
+updater.dispatcher.add_handler(MessageHandler(Filters.status_update.new_chat_members, greet_new_member))
+
+# start bot
+
 updater.start_polling()
+
 updater.idle()
